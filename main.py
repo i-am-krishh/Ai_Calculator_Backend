c2ab4a751ff8c8793668bea7ce53a0675512d1f5
Krushna Wagh <wagh.krushna2004@gmail.com>
2025-08-25 18:00:10 +0530

    cors errors

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI(lifespan=lifespan)
 
 
 origins = [
-    "https://ai-calculator-frontend-psi.vercel.app/",
+    "https://ai-calculator-frontend-psi.vercel.app",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
     "*"
